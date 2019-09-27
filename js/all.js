dea510e9b94a4a621c6e74eb7118e204487445f0
bhuvana-ship <incerd.bhuvana@gmail.com>
2019-09-27 15:44:55 +0530

    AngularJS

---
+++ js/all.js
@@ -0,0 +1,88 @@
+var app = angular.module("myApp", []);
+// app.controller("firstCtrl", function ($scope) {
+//     $scope.name="Bhuvana";
+//  });
+
+
+app.controller("tableData", function ($scope) {
+    $scope.studentData = [{
+        sno: "1",
+        name: "Bhuvana",
+        age: 28,
+        cell: 8073412306,
+        place: "Maruthi Nagar",
+        address: "Bangalore"
+    },
+    {
+        sno: "2",
+        name: "Vaishali",
+        age: 28,
+        cell: 8073412306,
+        place: "Madiwala",
+        address: "Bangalore"
+    },
+    {
+        sno: "3",
+        name: "Karishma",
+        age: 28,
+        cell: 8073412306,
+        place: "Jp Nagar",
+        address: "Bangalore"
+    },
+    {
+        sno: "4",
+        name: "Sharadha",
+        age: 28,
+        cell: 8073412306,
+        place: "BTM Layout",
+        address: "Bangalore"
+    },
+    {
+        sno: "5",
+        name: "Harish",
+        age: 28,
+        cell: 8073412306,
+        place: "Jayanagar",
+        address: "Bangalore"
+    },
+    {
+        sno: "6",
+        name: "Rajesh",
+        age: 28,
+        cell: 8073412306,
+        place: "Ramurthi Nagar",
+        address: "Bangalore"
+    },
+    {
+        sno: "7",
+        name: "Bachu",
+        age: 28,
+        cell: 8073412306,
+        place: "Maruthi Nagar",
+        address: "Bangalore"
+    },
+    {
+        sno: "8",
+        name: "Balu",
+        age: 28,
+        cell: 8073412306,
+        place: "Maruthi Nagar",
+        address: "Bangalore"
+    },
+    {
+        sno: "9",
+        name: "Nishanthi",
+        age: 28,
+        cell: 8073412306,
+        place: "Maruthi Nagar",
+        address: "Bangalore"
+    },
+    {
+        sno: "10",
+        name: "Pushpa",
+        age: 28,
+        cell: 8073412306,
+        place: "Maruthi Nagar",
+        address: "Bangalore"
+    }];
+})
